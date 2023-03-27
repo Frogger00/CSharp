285720bd0a59efbeece3d4e0787fb6593cc1d97d
Frogger00 <sebastianzarazua186@gmail.com>
2023-03-26 18:20:39 -0600

    commit

--- p48-tabla-de-conversion/Program.cs
+++ p48-tabla-de-conversion/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Imprime tabla de conversión de peso a dolar
 int ini, fin, c;
-float tc = 19.99f;
+float tc = 18.62f, tce = 19.95f;
 char resp;
 do {
     Console.Clear();
@@ -10,14 +10,14 @@ do {
         Console.Write("Fin : "); fin = int.Parse(Console.ReadLine());
     } while(fin < ini);
     c = ini;
-    Console.WriteLine(new string('-', 20));
-    Console.WriteLine("Peso\tDolar");
-    Console.WriteLine(new string('-', 20));
+    Console.WriteLine(new string('-', 25));
+    Console.WriteLine("Peso\tDolar\tEuro");
+    Console.WriteLine(new string('-', 25));
     while( c <= fin ) {
-        Console.WriteLine($"{c}\t{c * tc:f4}");
+        Console.WriteLine($"{c}\t{c / tc:f2}\t{c / tce:f2}");
         c++;
     }
-    Console.WriteLine(new string('-', 20));
+    Console.WriteLine(new string('-', 25));
     Console.Write("\nDeseas continuar (S/N) ? ");resp = char.ToUpper(Console.ReadLine()[0]);
 } while( resp != 'N');
 Console.WriteLine("\nGracias por utilizar este programa !");
